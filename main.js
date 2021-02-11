3d58679932751d919a5e55b101ef2bfaf2a8b20e
iradualbert <albert.iradukunda@yahoo.com>
2021-02-12 02:39:16 +0300

    Updates

---
+++ main.js
@@ -0,0 +1,11 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const hostname = window.location.host;
+//     if (hostname == "iradualbert.github.io") {
+//         const baseUrl = "https://iradualbert.github.io/portfolio/assets/"
+//         const images = document.querySelectorAll('img')
+//         for (const img of images) {
+//             const imgName = img.src.split('/')[4];
+//             img.src = baseUrl + imgName
+//         }
+//     }
+// });
